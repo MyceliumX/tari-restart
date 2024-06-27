80a015f8ad3df4330d94b05fbdf3c96175c87b9b
Meta Light <104095033+meta-light@users.noreply.github.com>
2024-06-27 12:39:30 -0500

    Update restart.bat

--- restart.bat
+++ restart.bat
@@ -10,6 +10,14 @@ timeout /t 3600 /nobreak
 rem Kill xtrgpuminer.exe process
 taskkill /f /im xtrgpuminer.exe
 
+rem Check if xtrgpuminer.exe is still running and wait for it to close
+:check_process
+tasklist /fi "IMAGENAME eq xtrgpuminer.exe" 2>NUL | find /I /N "xtrgpuminer.exe">NUL
+if "%ERRORLEVEL%"=="0" (
+    timeout /t 1 /nobreak
+    goto check_process
+)
+
 rem Wait for a few seconds before restarting
 timeout /t 5 /nobreak
 
